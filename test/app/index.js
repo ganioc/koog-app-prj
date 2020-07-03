f0a3b0ff7acd3c4705c2f77ab74b30474d6ce9e6
yangoe <o3.yang@hotmail.com>
2020-07-03 18:16:26 +0800

    Add umsc/getsecretstr.js

--- test/app/index.js
+++ test/app/index.js
@@ -1,4 +1,12 @@
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
+const getBalance = require('../../lib/umsc/getbalance')
+const getTimeStamp = require('../../lib/umsc/gettimestamp')
 
-console.log(appDir)
+// console.log(appDir)
+
+console.log('\ngetbalance test:')
+
+getTimeStamp()
+
+
---
+++ test/app/testmd5.js
@@ -0,0 +1,4 @@
+
+const getSecretStr = require('../../lib/umsc/getsecretstr')
+
+getSecretStr('J10003', '111111', '0803192020')
