0fd9d3f2ad8d0dbaa9062dfd9725e18984f06406
yangoe <o3.yang@hotmail.com>
2020-07-04 17:44:35 +0800

    test batch send is OK

--- test/app/testbatch.js
+++ test/app/testbatch.js
@@ -1,6 +1,6 @@
 const cfgObj = require('../../config/config.json')
 const faci = require('../../lib/facility')
-const reqSingleSend = require('../../lib/umsc/reqsinglesend')
+const reqBatchSend = require('../../lib/umsc/reqbatchsend')
 
 console.log('config:')
 console.log(cfgObj)
@@ -9,14 +9,14 @@ let platform = cfgObj.platform
 let userid = platform.ACCOUNT
 let pwd = platform.PWD
 
-reqSingleSend(false, userid,
+reqBatchSend(false, userid,
   pwd,
-  faci.getSingleSendUrl(platform),
-  '18621661947',
-  'Hello 123456',
+  faci.getBatchSendUrl(platform),
+  '18621661947,13041686656',
+  'Hello 你好 123456',
   (err, res, body) => {
     if (err) {
-      console.log('wrong single_send')
+      console.log('wrong batch_send')
     } else {
       // console.log('res:', res)
       console.log('body:', body)
